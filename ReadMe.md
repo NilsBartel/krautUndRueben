a55de037e6d65f102c82bf88fb6bd3a91891aae8
nilsbartel <nils.bartel@virtualminds.com>
2025-05-02 17:47:09 +0200

    added filter endpoint, and updated ReadMe.md

--- ReadMe.md
+++ ReadMe.md
@@ -5,9 +5,14 @@ Eine Datenbank mit frontend für eine Nahrungsbox.
 1. have docker running
 2. run the start-local-setup.sh in /local to get the setup the docker database
 3. run the initNewNew.sql in /src/main/resources/newProposal to setup the database
-4. run the dataWithBetterUnits.sql in /src/main/resources/newProposal to populate the databae
+4. run the testData.sql in /src/main/resources/newProposal to populate the databae
 5. run Main at /src/main/java/org.example.gymbrobox/GymBroBoxApplication
 
 Documentation:  
 1. run the program  
 http://localhost:8080/swagger-ui.html
+
+
+### Endpoints under construction:
+- /recipe -> returns test data
+- /recipe/filter -> returns test data
--- src/main/java/org/example/gymbrobox/api/RecipeController.java
+++ src/main/java/org/example/gymbrobox/api/RecipeController.java
@@ -1,11 +1,17 @@
 package org.example.gymbrobox.api;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import org.example.gymbrobox.Service.RecipeService;
 import org.example.gymbrobox.model.Recipe;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class RecipeController {
@@ -22,6 +28,34 @@ public class RecipeController {
         return recipeService.createRecipe();
     }
 
+    @Parameters({
+            @Parameter(name = "ernährungsart", description = "frutarisch, vegan, vegetarian, mischKost ", example = "vegan"),
+            @Parameter(name = "kohlenhydrate", description = "low, high", example = "low"),
+            @Parameter(name = "protein", description = "low, high", example = "high")
+    })
+    @GetMapping("/recipe/filter")
+    @ResponseBody
+    public List<Recipe> getFilteredRecipes(
+            @RequestParam(required = false) Map<String, String> queryParams
+    ) {
+
+
+        System.out.println();
+        for (Map.Entry<String, String> entry : queryParams.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+
+        //TODO: do the thing (Service, Repo)
+        List<Recipe> recipes = new ArrayList<>();
+        recipes.add(recipeService.createRecipe());
+        recipes.add(recipeService.createRecipe());
+        recipes.add(recipeService.createRecipe());
+
+
+        return recipes;
+
+    }
 
 
 }
