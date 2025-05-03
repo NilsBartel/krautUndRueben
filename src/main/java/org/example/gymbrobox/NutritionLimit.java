57f01698cbfce04e517c4a31129e9c86c784a7cc
nilsbartel <nils.bartel@virtualminds.com>
2025-05-03 16:31:23 +0200

    added NutritionLimit enum

---
+++ src/main/java/org/example/gymbrobox/NutritionLimit.java
@@ -0,0 +1,18 @@
+package org.example.gymbrobox;
+
+public enum NutritionLimit {
+    PROTEIN_LOW_LIMIT(20),
+    KOHELENHYDRATE_LOW_LIMIT(40),
+    FETT_LOW_LIMIT(20),
+    KALORIEN_LOW_LIMIT(500);
+
+    private final int value;
+
+    NutritionLimit(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
