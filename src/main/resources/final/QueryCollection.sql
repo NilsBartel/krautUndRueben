40917971e6de34fe5c89bad48e64dd96a363cfef
Joshi123456789 <j.behncke@prodware.de>
2025-05-05 12:28:59 +0200

    Updated Version of Query Collection

--- src/main/resources/final/QueryCollection.sql
+++ src/main/resources/final/QueryCollection.sql
@@ -1,58 +1,156 @@
-# Auswahl aller Zutaten eines Rezepts
-SELECT * FROM rezept_zutat rz
-JOIN zutat z
-ON rz.ZutatNr = z.ZutatNr
-# Hier beliebige RezeptNr einfügen
-WHERE rz.RezeptNr IN (1);
+# Auswahl aller Zutaten eines Rezepts nach RezeptNr
+SELECT * FROM zutat z
+JOIN rezept_zutat rz
+ON z.ZUTATNR = rz.ZUTATNR
+WHERE rz.REZEPTNR = 1; -- Hier beliebige RezeptNr einfügen
+
+# Auswahl aller Zutaten eines Rezepts nach Name
+SELECT * FROM zutat z
+JOIN rezept_zutat rz
+ON z.ZUTATNR = rz.ZUTATNR
+WHERE rz.REZEPTNR IN (
+	SELECT r.REZEPTNR FROM rezept r
+	WHERE r.`NAME` = 'Lachslasagne' -- Hier beliebigen Namen einfügen
+);
 
 # Auswahl aller Rezepte einer bestimmten Ernährungskategorie
-SELECT * FROM rezept r
-WHERE r.RezeptNr NOT IN (
-	SELECT rz.RezeptNr FROM rezept_zutat rz
-	WHERE rz.ZutatNr IN (
-		SELECT ze.ZUTATNR FROM zutat_ernaehrungskategorie ze
-		JOIN zutat z
-		ON ze.ZUTATNR = z.ZUTATNR
-		# Hier gewählte Ernährungskategorie angeben
-		WHERE ze.ERNAEHRUNGSKATNR != 4 AND ze.ERNAEHRUNGSKATNR IN (
-			SELECT e.ERNAEHRUNGSKATNR FROM ernaehrungskategorie e
-			# Hier Typ und Priorität der gewählten Ernährungskategorie angeben
-			WHERE e.TYP = 'ernährungsart' AND e.PRIORITAET < 1
-		)
-	)
+SELECT r.*
+FROM REZEPT r
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM REZEPT_ZUTAT rz
+    WHERE rz.REZEPTNR = r.REZEPTNR
+      AND rz.ZUTATNR NOT IN (
+        select z.ZUTATNR from ZUTAT z
+            join ZUTAT_ERNAEHRUNGSKATEGORIE ze on z.ZUTATNR = ze.ZUTATNR
+        where ze.ERNAEHRUNGSKATNR in (
+            SELECT e.ERNAEHRUNGSKATNR FROM ERNAEHRUNGSKATEGORIE e
+            WHERE e.PRIORITAET >= (
+                SELECT e2.PRIORITAET
+                FROM ERNAEHRUNGSKATEGORIE e2
+                WHERE e2.BEZEICHNUNG = 'frutarisch' -- the one to edit
+            ) AND e.TYP = 'ernährungsart'
+        )
+    )
 );
 
 # Rezepte nach verwendeten Zutaten filtern
-SELECT * FROM rezept r
+SELECT * FROM REZEPT r
 WHERE r.RezeptNr IN (
-	SELECT rz.RezeptNr FROM rezept_zutat rz
-	# Hier beliebige ZutatenNr angeben
-	WHERE rz.ZutatNr IN (1013, 1015)
+   SELECT rz.RezeptNr FROM REZEPT_ZUTAT rz
+	WHERE rz.ZutatNr IN (1001) -- Hier beliebige ZutatenNr angeben
 );
 
-# TODO - Berechnung der durschnittlichen Nährwerte einer Bestellung
+# Berechnung der durschnittlichen Nährwerte einer Bestellung
+DROP VIEW Totals;
+CREATE VIEW Totals AS
+SELECT SUM((z.Kalorien / 100) * rz.MENGE) AS Total FROM zutat z
+JOIN rezept_zutat rz
+ON z.ZUTATNR = rz.ZUTATNR
+WHERE rz.REZEPTNR IN (
+	SELECT br.REZEPTNR FROM bestellung_rezept br
+	WHERE br.BESTELLNR IN (1001) -- Hier beliebige BestellNr angeben
+)
+GROUP BY rz.REZEPTNR;
 
+SELECT AVG(t.Total) FROM Totals AS t;
 
-# Rezepte nach Kalorien Filtern
-SELECT * FROM rezept r
+# Die meistbestellten Rezepte
+SELECT br.REZEPTNR, COUNT(br.REZEPTNR) AS Haeufigkeit FROM bestellung_rezept br
+JOIN rezept r
+ON br.REZEPTNR = r.REZEPTNR
+GROUP BY br.REZEPTNR
+ORDER BY Haeufigkeit DESC;
+
+# Auswahl aller Rezepte, die eine bestimmte Kalorienmenge nicht überschreiten 
+SELECT
+    r.*
+FROM REZEPT r
 WHERE r.REZEPTNR IN (
-	SELECT rz.REZEPTNR FROM rezept_zutat rz
-	WHERE (
-		SELECT SUM(rz.MENGE * z.KALORIEN / 100) FROM zutat z
-		WHERE z.ZutatNr IN (
-			SELECT rz2.ZutatNr FROM rezept_zutat rz2
-			WHERE rz2.RezeptNr = rz.REZEPTNR
-		)
-	# Hier den gewünschten Kalorienfilter angeben
-	) > 100000
+    SELECT
+        r.REZEPTNR
+    FROM REZEPT r
+             JOIN REZEPT_ZUTAT rz ON r.REZEPTNR = rz.REZEPTNR
+             JOIN ZUTAT z ON rz.ZUTATNR = z.ZUTATNR
+    GROUP BY r.REZEPTNR
+    HAVING SUM((z.KALORIEN / 100.0) * rz.MENGE) <= 700
 );
 
-# TODO - Durchschnittliche Nährwerte einer Box
-SELECT * FROM 
+# Auswahl aller Rezepte, die weniger als fünf Zutaten enthalten 
+SELECT
+    r.*
+FROM REZEPT r
+WHERE r.REZEPTNR IN (
+    SELECT
+        r.REZEPTNR
+    FROM REZEPT r
+             JOIN REZEPT_ZUTAT rz ON r.REZEPTNR = rz.REZEPTNR
+             JOIN ZUTAT z ON rz.ZUTATNR = z.ZUTATNR
+    GROUP BY r.REZEPTNR
+    HAVING COUNT(DISTINCT rz.ZUTATNR) < 6
+);
+
+# Auswahl aller Rezepte, die weniger als fünf Zutaten enthalten und eine bestimmte Ernährungskategorie erfüllen
+SELECT r.*
+FROM REZEPT r
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM REZEPT_ZUTAT rz
+    WHERE rz.REZEPTNR = r.REZEPTNR
+      AND rz.ZUTATNR NOT IN (
+        SELECT z.ZUTATNR FROM ZUTAT z
+              JOIN ZUTAT_ERNAEHRUNGSKATEGORIE ze ON z.ZUTATNR = ze.ZUTATNR
+        WHERE ze.ERNAEHRUNGSKATNR IN (
+            SELECT e.ERNAEHRUNGSKATNR FROM ERNAEHRUNGSKATEGORIE e
+            WHERE e.PRIORITAET >= (
+                SELECT e2.PRIORITAET
+                FROM ERNAEHRUNGSKATEGORIE e2
+                WHERE e2.BEZEICHNUNG = 'frutarisch' -- 'frutarisch', 'vegan', 'vegetarisch', 'fleisch essend'
+            ) AND e.TYP = 'ernährungsart'
+        )
+    )
+) AND r.REZEPTNR IN (
+    SELECT
+        r.REZEPTNR
+    FROM REZEPT r
+             JOIN REZEPT_ZUTAT rz ON r.REZEPTNR = rz.REZEPTNR
+             JOIN ZUTAT z ON rz.ZUTATNR = z.ZUTATNR
+    GROUP BY r.REZEPTNR
+    HAVING COUNT(DISTINCT rz.ZUTATNR) < 6 -- belibige zutaten anzahl
+);
+
+# Weitere Statements:
+
+-- zeige alle kunden mit ihren bestellungen an
+SELECT
+    k.KUNDENNR,
+    k.VORNAME,
+    k.NACHNAME,
+    b.BESTELLNR,
+    b.BESTELLDATUM
+FROM
+    KUNDE k
+        LEFT JOIN
+    BESTELLUNG b ON k.KUNDENNR = b.KUNDENNR;
+    
+
+    
 
 
 
-
+-- LACHSLASAGNE KALORIEN
+-- spinat: 46
+-- lachs: 250
+-- nudeln: 60
+-- mehl: 4,5
+-- sahne: 75
+-- parmesan: 14
+-- zitrone: 12
+-- käse: 148
+-- milch: 40
+-- buttler: 89
+-- brühe: 0,3
+-- = ca. 738,5
 
 
 
